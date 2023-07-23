99bd5478786d92e16aa09a2827340128a10498df
Smaddermand <50303469+Smaddermand@users.noreply.github.com>
2023-07-23 16:08:06 +0200

    Create display function and css style

--- script.js
+++ script.js
@@ -3,7 +3,70 @@
 
 let number1 = 0;
 let number2 = 0;
-let operator1 = "";
+let operatorSelection = "";
+let displayValue ="";
+let resultFlag = false;
+
+
+
+// Get all the buttons with the class "btn"
+const buttons = document.querySelectorAll('.btn');
+
+// Get the output div element
+const outputDiv = document.getElementById('output'); 
+
+
+// Function to handle button click
+function handleButtonClick(event) {
+  // Get the clicked button
+  const clickedButton = event.target;
+
+  // Get the text content of the clicked button
+  const buttonText = clickedButton.textContent;
+
+  // Display the text content in the output div
+  displayOutput(buttonText);
+}
+
+// Function to display the output in the output div
+function displayOutput(text) {
+  // If 'AC' is pressed, clear the output div
+  if (text === 'AC') {
+    outputDiv.textContent = '';
+    number1 = '';
+    number2 = '';
+    resultFlag = false;
+  } else if (text === '+' || text === '-' || text === '/' || text === '*'){
+    operatorSelection = text;
+    number1 = outputDiv.textContent;
+    outputDiv.textContent = '';
+    resultFlag = false;
+  } else if(text === '='){
+    //operate
+    number2 = outputDiv.textContent;
+    number1 = Number(number1);
+    number2 = Number(number2);
+    outputDiv.textContent = operate(number1, number2, operatorSelection);
+    number1 = number2;
+    number2 = 0;
+    resultFlag = true;
+
+  } else  {
+    // Otherwise, append the text to the existing content
+    if(!resultFlag){
+        outputDiv.textContent += text;
+    }
+  }
+
+}
+
+// Attach click event listener to each button
+buttons.forEach(button => {
+  button.addEventListener('click', handleButtonClick);
+});
+
+
+//======OPERATOR FUNCTIONS======
 
 const operate = function(a, b, operator){
     if(operator === "+"){
@@ -17,10 +80,7 @@ const operate = function(a, b, operator){
     } else {
         return "SYNTAX ERROR"
     }
-}
-
-
-//======OPERATOR FUNCTIONS======
+};
 
 const add = function (a, b) {
   return a + b;
@@ -39,16 +99,3 @@ const divide = function (a, b) {
 };
 
 
-//============
-
-let result = operate(2, 4, "+");
-console.log(result);
-
-let result2 = operate(2, 4, "-");
-console.log(result2);
-
-let result3 = operate(3, 7, "*");
-console.log(result3);
-
-let result4 = operate(6, 2, "/");
-console.log(result4);
